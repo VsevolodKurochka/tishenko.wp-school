181aff7b6390553acb9761ecf23429aedafbfa0a
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-10-19 10:11:54 +0300

    Update timber library, functions

--- tgm/connect.php
+++ tgm/connect.php
@@ -62,10 +62,10 @@ function visotskiy_register_required_plugins() {
 		// This is an example of how to include a plugin bundled with a theme.
 		array(
 			'name'               => 'Timber', // The plugin name.
-			'slug'               => 'timber-library.1.7.1', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/tgm/plugins/timber-library.1.7.1.zip', // The plugin source.
+			'slug'               => 'timber-library.1.8.1', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/tgm/plugins/timber-library.1.8.1.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '1.7.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'version'            => '1.8.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
